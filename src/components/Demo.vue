5682826b3aef9f2aa103468a8edf6f5afdb1fd40
yjy1 <318277446@qq.com>
2023-02-15 21:50:23 +0800

    tag_watch监视reactive定义的数据

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -4,53 +4,74 @@
     <hr>
     <h2>当前的信息为：{{ msg }}</h2>
     <button @click="msg += '!'">修改当前信息</button>
+    <hr>
+    <h2>姓名：{{person.name}}</h2>
+    <h2>年龄：{{person.age}}</h2>
+    <h2>薪资：{{person.job.j1.salary}}K</h2>
+    <button @click="person.name += '~'">修改姓名</button>
+    <button @click="person.age ++ ">年龄增长</button>
+    <button @click="person.job.j1.salary ++ ">涨薪</button>
 </template>
 
 <script>
-import { ref ,watch } from 'vue'
+import {ref, reactive ,watch } from 'vue'
 export default {
     name: 'Demo',
-    // vue2写法
-    // watch:{
-    //    /*  sum(newValue,oldValue){
-    //         console.log('sum被改变了',newValue,oldValue)
-    //     } */
-    //     sum:{
-    //         immediate:true,
-    //         deep:true,
-    //         handler(newValue,oldValue){
-    //             console.log('sum被改变了',newValue,oldValue)
-    //         }
-    //     }
-    // },
     setup() {
         let sum = ref(0)
         let msg = ref('你好啊')
+
+        let person = reactive({
+            name:'张三',
+            age:18,
+            job:{
+                j1:{
+                    salary:20
+                }
+            }
+        })
         // 监视
         //情况一:监视ref所定义的一个响应式数据
-        watch(sum,(newValue,oldValue)=>{
+       /*  watch(sum,(newValue,oldValue)=>{
             console.log('sum被改变了',newValue,oldValue)
         },{
             immediate:true 
         })
-
+ */
         //情况二:监视ref所定义的多个响应式数据
-      /*   watch(sum,(newValue,oldValue)=>{
-            console.log('sum被改变了',newValue,oldValue)
-        })
-        watch(msg,(newValue,oldValue)=>{
-            console.log('msg被改变了',newValue,oldValue)
-        }) */
-        
-        watch([sum,msg],(newValue,oldValue)=>{
+       /*  watch([sum,msg],(newValue,oldValue)=>{
             console.log('sum或msg被改变了',newValue,oldValue)
         },{
             immediate:true 
-        })
+        }) */
+
+       /*    
+            情况三:监视reactive所定义的一个响应式数据的全部属性,
+            注意：1.此处无法正确获取oldValue 
+                2.强制开启深度监视（deep配置无效）
+       */
+      /*   watch(person,(newValue,oldValue)=>{
+            console.log('监视reactive',newValue ,oldValue)
+        },{deep:false})  //此处的deep配置无效*/
+
+        // 情况四:监视reactive所定义的一个响应式数据的某个属性
+        /* watch(()=>person.age,(newValue,oldValue)=>{
+            console.log('监视reactive',newValue ,oldValue)
+        }) */
+
+        // 情况五:监视reactive所定义的一个响应式数据的某些属性
+       /*  watch([()=>person.name,()=>person.age],(newValue,oldValue)=>{
+            console.log('监视reactive',newValue ,oldValue)
+        }) */
+        // 特殊情况
+        watch(()=>person.job,(newValue,oldValue)=>{
+            console.log('监视job',newValue ,oldValue)
+        },{deep:true }) //此处由于监视的是reactive素定义的对象中的某个属性，所以deep配置有效
         // 返回一个对象（常用）
         return {
             sum,
-            msg
+            msg,
+            person
         }
     }
 }
