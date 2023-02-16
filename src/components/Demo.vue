698da43f625f970f657eb7a63bc33bf9119c3030
yjy1 <318277446@qq.com>
2023-02-16 23:15:15 +0800

    tag_toRaw与markRaw

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,13 +5,19 @@
     <h2>姓名：{{ name }}</h2>
     <h2>年龄：{{ age }}</h2>
     <h2>薪资：{{ job.j1.salary }}K</h2>
+    <h2 v-show="person.car">座驾信息：{{ person.car }}</h2>
     <button @click="name += '~'">修改姓名</button>
     <button @click="age++">年龄增长</button>
-<button @click="job.j1.salary++">涨薪</button>
+    <button @click="job.j1.salary++">涨薪</button>
+    <button @click="showRawPerson">输出最原始的person</button>
+    <button @click="addCar">给人添加一台车</button>
+    <button @click="person.car.name+='!'">换车名</button>
+    <button @click="person.car.price++">换价格</button>
+    <button @click="changePrice">换价格2</button>
 </template>
 
 <script>
-import {ref, reactive, toRef ,toRefs,readonly,shallowReadonly} from 'vue'
+import {ref, reactive, toRef ,toRefs,toRaw,markRaw} from 'vue'
 export default {
     name: 'Demo',
     setup() {
@@ -25,12 +31,29 @@ export default {
                 }
             }
         }) 
-        // person = readonly(person)
-        person = shallowReadonly(person)
-        sum = readonly(sum)
+        function showRawPerson(){
+            // let p = toRaw(person)
+            // p.age++
+            // console.log(p)
+            
+            sum = toRaw(sum)
+            console.log(sum)
+        }
+        function addCar(){
+            let car = {name:'奔驰',price:'40'}
+            person.car = markRaw(car)
+        }
+        function changePrice(){
+            person.car.price++
+            console.log(person.car.price)
+        }
         return{
+            person,
             ...toRefs(person),
-            sum
+            sum,
+            showRawPerson,
+            addCar,
+            changePrice
         }
     }
 }
